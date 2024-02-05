bebcba679954910e441bddf763ece6b8427ecddf
Frank Lyonnet <flyonnet@edamame.tech>
2024-02-05 18:00:44 +0100

    Initial

---
+++ build.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    // Dotenv build with a specific env path
+    let config = dotenv_build::Config {
+        filename: std::path::Path::new("../secrets/lambda-signature.env"),
+        recursive_search: false,
+        fail_if_missing_dotenv: false,
+        ..Default::default()
+    };
+    dotenv_build::output(config).unwrap();
+}
