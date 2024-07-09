fd945ddef05c4c7c2789bcd28076da25a7b9c21f
Frank Lyonnet <flyonnet@edamame.tech>
2024-07-09 11:08:15 +0200

    CHORES: format

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-pub mod signature;
 pub mod history_backend;
 pub mod lanscan_device_info_backend;
 pub mod lanscan_dislike_device_info_backend;
@@ -8,6 +7,5 @@ pub mod order_backend;
 pub mod order_type_backend;
 pub mod pwned_breach_backend;
 pub mod score_backend;
+pub mod signature;
 pub mod threat_backend;
-
-
