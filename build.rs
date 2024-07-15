b39eaa3dde7ba98a5d7009aca04348dfab66aa68
Frank Lyonnet <flyonnet@edamame.tech>
2024-07-15 12:45:44 +0200

    ADD: makefile

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    // Dotenv build with a specific env path
-    let config = dotenv_build::Config {
-        filename: std::path::Path::new("../secrets/lambda-signature.env"),
-        recursive_search: false,
-        fail_if_missing_dotenv: false,
-    };
-    dotenv_build::output(config).unwrap();
-}
