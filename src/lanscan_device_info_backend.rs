cd65eae9293085cbec0779593b671103d4f88425
Frank Lyonnet <flyonnet@edamame.tech>
2025-04-14 18:50:17 +0200

    ADD: implement uid methods for DeviceInfoBackend, BreachDetailBackend, and SessionInfoBackend using Blake3 hasher

--- src/lanscan_device_info_backend.rs
+++ src/lanscan_device_info_backend.rs
@@ -1,15 +1,30 @@
 use crate::lanscan_port_info_backend::PortInfoBackend;
 use crate::lanscan_vulnerability_info_backend::VulnerabilityInfoBackend;
+use blake3::Hasher;
 use serde::{Deserialize, Serialize};
 
-// Simplified version for use with the backend
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct DeviceInfoBackend {
-    // PII
     pub mdns_services: Vec<String>,
-    // Non-PII
     pub device_vendor: String,
     pub vulnerabilities: Vec<VulnerabilityInfoBackend>,
-    // Sorted Vec would be better but we had trouble with the bridge once...
     pub open_ports: Vec<PortInfoBackend>,
+}
+
+impl DeviceInfoBackend {
+    pub fn uid(&self, language: &str) -> String {
+        let mut hasher = Hasher::new();
+        hasher.update(language.as_bytes());
+        hasher.update(self.device_vendor.as_bytes());
+        // Use a pipe delimiter to avoid collisions
+        hasher.update(self.mdns_services.join("|").as_bytes());
+        // Vulnerabilities have a possibility of change, so we include it
+        let mut sorted_vulnerabilities = self.vulnerabilities.clone();
+        sorted_vulnerabilities.sort_by(|a, b| a.name.cmp(&b.name));
+        hasher.update(format!("{:?}", sorted_vulnerabilities).as_bytes());
+        let mut sorted_open_ports = self.open_ports.clone();
+        sorted_open_ports.sort_by(|a, b| a.port.cmp(&b.port));
+        hasher.update(format!("{:?}", sorted_open_ports).as_bytes());
+        hasher.finalize().to_hex().to_string()
+    }
 }
--- src/session_info_backend.rs
+++ src/session_info_backend.rs
@@ -1,3 +1,4 @@
+use blake3::Hasher;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -14,4 +15,40 @@ pub struct SessionInfoBackend {
     pub l7_process_name: Option<String>,
     pub l7_process_path: Option<String>,
     pub l7_process_user: Option<String>,
+}
+
+impl SessionInfoBackend {
+    pub fn uid(&self, language: &str) -> String {
+        let mut hasher = Hasher::new();
+        hasher.update(language.as_bytes());
+        hasher.update(self.dst_ip.as_bytes());
+        hasher.update(self.dst_port.to_string().as_bytes());
+        hasher.update(self.protocol.as_bytes());
+        if let Some(dst_domain) = &self.dst_domain {
+            hasher.update(dst_domain.as_bytes());
+        }
+        if let Some(dst_asn_number) = self.dst_asn_number {
+            hasher.update(dst_asn_number.to_string().as_bytes());
+        }
+        if let Some(dst_asn_country) = &self.dst_asn_country {
+            hasher.update(dst_asn_country.as_bytes());
+        }
+        if let Some(dst_asn_owner) = &self.dst_asn_owner {
+            hasher.update(dst_asn_owner.as_bytes());
+        }
+        hasher.update(self.criticality.as_bytes());
+        if let Some(dst_service) = &self.dst_service {
+            hasher.update(dst_service.as_bytes());
+        }
+        if let Some(l7_process_name) = &self.l7_process_name {
+            hasher.update(l7_process_name.as_bytes());
+        }
+        if let Some(l7_process_path) = &self.l7_process_path {
+            hasher.update(l7_process_path.as_bytes());
+        }
+        if let Some(l7_process_user) = &self.l7_process_user {
+            hasher.update(l7_process_user.as_bytes());
+        }
+        hasher.finalize().to_hex().to_string()
+    }
 }
