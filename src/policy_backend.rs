e5b68102f5d87ab8410e2ed7c2c8e0850a1bf101
EDAMAME Dev Local <dev@edamame.tech>
2025-05-19 20:31:16 +0200

    UPDATE: change reason field type in PoliciesStatusBackend from Option to Vec for improved flexibility and update comments for clarity

--- src/policy_backend.rs
+++ src/policy_backend.rs
@@ -4,39 +4,39 @@ use serde::{Deserialize, Serialize};
 pub struct PoliciesStatusBackend {
     name: String,
     passed: bool,
-    reason: Option<ReasonBackend>,
+    reason: Vec<ReasonBackend>,
     providers: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, PartialOrd)]
 pub enum ReasonBackend {
-    /// Minimum score has not been respected
+    // Minimum score has not been respected
     MinScoreNotRespectedBackend {
-        /// Required overall score
+        // Required overall score
         required: u8,
 
         // Device's reported overall score
         got: u8,
     },
 
-    /// At least one required security check does not pass
+    // At least one required security check does not pass
     SecurityChecksNotPassedBackend {
-        /// Required security checks
+        // Required security checks
         required: Vec<String>,
 
-        /// Device's passed security checks (subset of required only, not all the passed)
+        // Device's passed security checks (subset of required only, not all the passed)
         passed: Vec<String>,
 
-        /// Device's failed security checks (subset of required only, not all the failed)
+        // Device's failed security checks (subset of required only, not all the failed)
         failed: Vec<String>,
     },
 
-    //
+    // Failed tag
     TagsNotRespectedBackend {
-        /// failed tag
+        // failed tag
         required: String,
 
-        /// Device's ratio tag
+        // Device's ratio tag
         got: f64,
     },
 }
